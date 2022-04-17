7a0ba5d32fd2840222a685333c02c2f9a4fb866c
MarinaDreamwork <84763787+MarinaDreamwork@users.noreply.github.com>
2022-04-17 21:55:30 +0300

    Connection and setup Redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { useSelector } from 'react-redux';
 
 function App() {
+  const store = useSelector(store => store);
+  console.log('store', store);
   return (
     <div className="App">
       Привет! Стартовало выполнение тестового задания!
